293596ad691c2acc81e77c27ad102e2b9788a200
thayscasado <thayscasado@hotmail.com>
2023-04-15 19:15:21 -0700

    added migrations setting to create and roll back, also creating routes for returning data from the database

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
 
 app.get('/', async (request, reply) => {
-  const test = await knex('sqlite_schema').select('*');
-  return test;
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test transaction',
+      amount: 1000,
+    })
+    .returning('*');
+
+  const selectQuery = knex('transactions')
+    .where('amount', '<', 500)
+    .select('*');
+
+  return selectQuery;
 });
 
 app
